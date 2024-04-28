b8cc48ded646494803c201d9ec4e890ddeb53dd7
cfzimmerman <coryzimmerman93@gmail.com>
2024-04-28 14:51:32 -0400

    really basic h1 to h2 packet forwarding

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,50 @@
-use pnet::datalink;
+use anyhow::bail;
+use pnet::{
+    datalink::{self, Channel::Ethernet, Config, NetworkInterface},
+    packet::{
+        ethernet::{EthernetPacket, MutableEthernetPacket},
+        MutablePacket, Packet,
+    },
+};
 
 struct EthRouter {}
 
 impl EthRouter {
     pub fn build() -> anyhow::Result<Self> {
-        for intf in datalink::interfaces()
+        // filters out all ethernet interfaces that don't have mininet names
+        let mn_intf: Vec<NetworkInterface> = datalink::interfaces()
             .into_iter()
             .filter(|intf| intf.name.contains("-eth"))
-        {
-            println!("{:#?}", intf);
+            .collect();
+
+        let Ok(Ethernet(_i1_tx, mut i1_rx)) = datalink::channel(&mn_intf[0], Config::default())
+        else {
+            bail!(
+                "Failed to parse ethernet channel on interface: {:#?}",
+                mn_intf[0]
+            );
+        };
+
+        let Ok(Ethernet(mut i2_tx, _i2_rx)) = datalink::channel(&mn_intf[1], Config::default())
+        else {
+            bail!(
+                "Failed to parse ethernet channel on interface: {:#?}",
+                mn_intf[1]
+            );
+        };
+
+        while let Ok(i1_pkt) = i1_rx.next() {
+            let Some(eth_pkt) = EthernetPacket::new(i1_pkt) else {
+                continue;
+            };
+
+            i2_tx.build_and_send(1, eth_pkt.packet().len(), &mut |outbound| {
+                let mut outbound = MutableEthernetPacket::new(outbound)
+                    .expect("MutableEthernetPacket must construct successfully");
+                outbound.clone_from(&eth_pkt);
+            });
         }
+
         Ok(EthRouter {})
     }
 }
