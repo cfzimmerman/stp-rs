55a98e0d338a71dcb604f43d5e175249944139c9
cfzimmerman <coryzimmerman93@gmail.com>
2024-04-29 20:58:23 -0400

    expanded topology

--- run.py
+++ run.py
@@ -33,12 +33,15 @@ class EtherTopo(Topo):
 
         h1 = self.addHost('h1')
         h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
 
         self.addLink(h1, s1)
+        self.addLink(h2, s2)
+        self.addLink(h3, s3)
+
         self.addLink(s1, s2)
         self.addLink(s1, s3)
         self.addLink(s2, s3)
-        self.addLink(s3, h2)
 
         # s1 = self.addSwitch('s1', cls=EtherSwitch)
 
